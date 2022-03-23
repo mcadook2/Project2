68f43abd5fc7767c7bcb41fefbca9d786efbb7bb
Fynn McAdoo <97468002+mcadook2@users.noreply.github.com>
2022-03-23 11:39:37 -0400

    Add files via upload

---
+++ Board.cc
@@ -0,0 +1,86 @@
+#include<Board.h>
+#include<curses.h>
+
+#define MAXCHARS 10
+#define BALL_CHARACTER 'O'
+
+const int COLOR_LEVELS[MAXCHARS] = {COLOR_WHITE, COLOR_WHITE, COLOR_YELLOW,
+                                     COLOR_GREEN, COLOR_GREEN, COLOR_CYAN,
+                                     COLOR_BLUE, COLOR_BLUE, COLOR_MAGENTA,
+                                     COLOR_RED};
+
+
+//Sets the x and y position, dx and dy velocity, and colorlevel
+void Ball::SetBallInfo(double newx, double newy, int newcolorlevel, double newDX, double newDY) {
+        x = newx; 
+        y = newy;
+        dx = newDX;
+        dy = newDY;
+        colorlevel = newcolorlevel;
+    }
+
+double Ball::GetX() const { 
+  return x; //returns the x postion
+  }
+
+double Ball::GetY() const { 
+  return y; //returns the y position
+  }
+
+double Ball::GetColorLvl() const { 
+  return colorlevel; //returns the color level position
+  }
+
+double Ball::GetDX() const {
+  return dx; //returns the dx velocity
+}
+
+double Ball::GetDY() const {
+  return dy; //returns the dy velocity
+}
+
+void Ball::SetupDisplay(int &width, int &height, int id) {
+    initscr(); //ncurses: Determines the type of terminal, setup ncurses
+    cbreak(); //ncurses: Disable line buffering
+    noecho(); //ncurses: Don't echo input back to the screen
+
+    //Set the width and heigth of the field
+    height = LINES - 1;
+    width = COLS - 1;
+
+    //Clear the display
+    clear();
+
+    //Setup the colors
+    start_color();
+    
+        init_pair(id+1, COLOR_LEVELS[id], COLOR_BLACK);
+
+    refresh(); //ncurses: refresh the screen
+}
+
+void Ball::ClearDisplay() {
+    clear();
+}
+
+void Ball::ShutdownDisplay(){
+    endwin();
+}
+
+void Ball::DrawBall(double oldX, double oldY, double newX, double newY, int ballColorLevel) {
+  // If the old ball position is positive, erase it
+  if ( (oldX > 0) && (oldY > 0) ) {
+    //attron(COLOR_PAIR(0));  // Set color to Black
+    //mvaddch((int)oldY, (int)oldX, ' ');  // ncurses:  draw a blank space
+    attron(COLOR_PAIR(MAXCHARS-1));      // Set color to Red
+    mvaddch((int)oldY, (int)oldX, '.');  // ncurses:  draw a tracer as a '.'
+  }
+
+  // If the new ball position is positive, draw it
+  if ( (newX > 0) && (newY > 0) ) {
+    attron(COLOR_PAIR(ballColorLevel));  // Set color to whatever we're given
+    mvaddch((int)newY, (int)newX, BALL_CHARACTER);  // ncurses:  draw character
+  }
+
+  refresh();  // ncurses: refresh the screen
+}
---
+++ Main.cc
@@ -0,0 +1,132 @@
+#include<iostream>
+#include<Board.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+
+using namespace std;
+
+//For functions that violate the ANSI standard
+//we use windows.h but the rest uses unistd.h
+#ifdef _WIN32
+#include <Windows.h>
+#else <unistd.h>
+#endif
+
+void StepBall(double &ballsY, double &ballsY, double &ballDX,
+                double &ballDY, int ballColor, int width, int height) {
+    double ballsX = ballsX + ballDX; //moves the ball forward/backwards by dx
+    double ballsY = ballsY + ballDY; //moves the ball up/down by dy
+
+    if (ballsX < 1) {
+        ballsX = 1; //put it at the wall
+        ballDX *= -1.0; //bounces ball
+    }
+
+    if (ballsX > width) {
+        ballsX = width; //put it at the wall
+        ballDX *=-1.0; //bounces ball
+    }
+
+    if (ballsY < 1) {
+        ballsY = 1; //put it at the wall
+        ballDY *= -1.0; //bounces ball
+    }
+    if (ballsY > height) {
+        ballsY = height; //put it at the wall
+        ballDY *= -1.0;
+    }
+
+    ballX = ballsX;
+    ballY = ballsY;
+ }
+
+void MillisecondsDelay(int millisec) {
+    #ifdef _Win32
+        Sleep(millisec) //Takes milliseconds
+    #else
+        usleep(millisec*1000) //Takes nanoseconds
+    #endif
+}
+
+int main() {
+
+    int ballNum;
+    double ballsx = 10;
+    double ballsy = 10;
+    double ballsDX = 1;
+    double ballsDY = 1;
+    double ballscolor = 2;
+
+    int width;
+    int height;
+    int maxSteps = 500;
+    int count = 0;
+
+    vector<Ball> BallList;
+    Ball ballsball;
+    Ball currball;
+
+    cout << "How many balls do you want to create?";
+    cin >> ballNums;
+    cout << ballNums << endl;
+
+    do {
+
+        cout << "What is the initial X and Y position of ball"<< count + 1 << "? ";
+        cin >> ballsx >> ballsy;
+        cout << "What is the initial dX and and dY velocity of the ball" << count + 1 << "? ";
+        cin >> ballsDX >> ballsDY;
+        cout << "What is the color level of the ball" << count + 1 << "? ";
+        cin >> ballscolor;
+
+        ballsball.SetBallInfo(ballsx, ballsy, ballscolor, ballsDX, ballsDY);
+        BallList.push_back(ballsball);
+
+        ++count;
+
+    } while (count != ballNums);
+
+    //clears the display for the ASCII art
+    myBall.SetupDisplay(width, height, ballColor);
+    
+    for (int ball = 0; ball < ballNums; ball++) { 
+
+        currball = BallList.at(ball);
+        ballsX = currBall.GetX();
+        ballsY = currBall.GetY();
+        ballColor = currBall.GetColorLvl();
+        
+        myBall.DrawBall(-1, -1, ballsX, ballsY, ballColor);
+        getchar();
+    }
+
+    for(int step = 0; step<maxSteps; step++) {
+        for (int ball = 0; ball < ballNums; ball++) { 
+
+            currball = BallList.at(ball));
+            ballsX = currBall.GetX();
+            ballsY = currBall.GetY();
+            ballsDX = currBall.GetDX();
+            ballsDY = currBall.GetDY();
+            ballColor = currBall.GetColorLvl();
+            
+            MillisecondsDelay(50);
+
+            
+            double oldX = ballsX;
+            double oldY = ballsY;
+
+            StepBall(ballsX, ballsY, ballDX, ballDY, ballColor,
+            width, height);
+            myBall.DrawBall(oldX, oldY, ballsX, ballsY, ballColor);
+        }
+    }
+
+    myBall.ShutdownDisplay();
+
+    cout << "...Program Ended." << endl;
+    cout << "Display Dim: " << width << " " << height << endl;
+
+    return 0;
+}
