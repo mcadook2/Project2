c03648a6915a82edecd229d198ee8ad287a9e270
Fynn McAdoo <97468002+mcadook2@users.noreply.github.com>
2022-03-24 00:03:32 -0400

    Delete the class type

--- Board.h
+++ Board.h
@@ -1,21 +1,11 @@
 #ifndef BOARD_H_
 #define BOARD_H_
 
-class Ball {
-    protected:    
-    int x = 10;
-    int y = 10;
-    int colorlevel = 2;
-    double ballDX = 1;
-    double ballDY = 1;
-    public:
         void addBall()
         void SetupDisplay(int &width, int &height);
         void ClearDisplay();
         void ShutdownDisplay();
         void DrawBall(double oldX, double oldY, double newX,
         double newY, int ballColorLevel);
-
-}
 
 #endif
