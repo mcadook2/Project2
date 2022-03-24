fbf402d3db622f6c12d1e267a4c7877de84bebf8
Fynn McAdoo <97468002+mcadook2@users.noreply.github.com>
2022-03-24 01:02:25 -0400

    Update Board.h

--- Board.h
+++ Board.h
@@ -15,7 +15,7 @@ class Ball {
     double GetDX() const;
     double GetDY() const;
     double GetColorLvl() const;
-    void SetupDisplay(int &width, int &height);
+    void SetupDisplay(int &width, int &height, int colorlevel);
     void ClearDisplay();
     void ShutdownDisplay();
     void DrawBall(double oldX, double oldY, double newX, double newY, int ballsColor);
