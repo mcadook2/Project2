973caf120d3c7236ddcdfd5d9443f16014fa86a8
Fynn McAdoo <97468002+mcadook2@users.noreply.github.com>
2022-03-24 01:48:55 -0400

    Update Main.cc

--- Main.cc
+++ Main.cc
@@ -108,7 +108,13 @@ int main() {
       for(int step = 0; step<maxSteps; step++) {
          for (int b = 0; b<ballNums; b++) {   
             //MillisecondsDelay(50);
-          cout << "Moving ";
+        currBall = BallList[a];
+        ballsX = currBall.GetX();
+        ballsY = currBall.GetY();
+        ballsColor = currBall.GetColorLvl();
+        ballsDX = currBall.GetDX();
+        ballsDY = currBall.GetDY();
+        cout << "Moving " << currBall;
             
             //double oldX = ballsX;
             //double oldY = ballsY;
