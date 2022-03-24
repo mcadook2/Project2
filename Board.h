dec6ace216991f056028732654a94284b4ea35a2
Fynn McAdoo <97468002+mcadook2@users.noreply.github.com>
2022-03-24 00:09:55 -0400

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,11 +1,26 @@
 #ifndef BOARD_H_
 #define BOARD_H_
 
-        void addBall()
+class Ball {
+    protected:    
+    int x = 10;
+    int y = 10;
+    int colorlevel = 2;
+    double ballDX = 1;
+    double ballDY = 1;
+        void addBall(double newx, double newy, int newcolorlevel, double newDX, double newDY);
+    public:
+        double GetX const();
+        double GetY const();
+        double GetDX const();
+        double GetDY const();
+        double GetColorLvl const();
         void SetupDisplay(int &width, int &height);
         void ClearDisplay();
         void ShutdownDisplay();
         void DrawBall(double oldX, double oldY, double newX,
         double newY, int ballColorLevel);
+
+}
 
 #endif
