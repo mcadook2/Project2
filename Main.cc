64bce2396c8a767965f095ae5d4ea7d61834f552
Fynn McAdoo <97468002+mcadook2@users.noreply.github.com>
2022-03-24 01:02:59 -0400

    Update Main.cc

--- Main.cc
+++ Main.cc
@@ -96,7 +96,7 @@ int main() {
       
         
         //clears the display for the ASCII art
-        currBall.SetupDisplay(width, height);
+        currBall.SetupDisplay(width, height, ballsColor);
       
         currBall.DrawBall(-1, -1, ballsX, ballsY, ballsColor);
         getchar();
